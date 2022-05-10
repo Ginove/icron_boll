6558bed4d28aaf7bf0f71ed2cddd659d8b6351e8
Ginove <a997678485@outlook.com>
2022-05-10 09:38:52 +0800

    Update cron.py

--- cron.py
+++ cron.py
@@ -1,3 +1,12 @@
-import os
-
-print("xxxxxxxxxxx")
+#codeing:utf-8
+import requests
+# 请求url
+url = "http://www.baidu.com"
+# 请求参数
+payload = {
+    "mobilephone":"1530272****",
+    "pwd":"123456"
+}
+# form表单形式，参数用data
+res = requests.post(url, data=payload)
+print(res.text)
